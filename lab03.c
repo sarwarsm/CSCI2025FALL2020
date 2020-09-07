f1484786e26ff11821d73a4c6f57ca2ab3c24219
sarwar <sohel.sarwar@sbcglobal.net>
2020-09-06 23:35:58 -0400

    Added c and h files

---
+++ lab03.c
@@ -0,0 +1,22 @@
+/*
+ * lab03.c
+ *
+ *  Created on: Sep 4, 2020
+ *      Author: sohel
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "stack.h"
+
+#define MIN_NUM_OF_OPERANDS 2
+
+extern int menuOriginal();
+extern int menuSwitch();
+extern int menuIfElse();
+
+int main(){
+	menuSwitch();
+}
